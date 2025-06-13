3c7b45d45ef687818f5681abcc7b1b083e352075
KarenKrill <vampart1997@gmail.com>
2025-06-13 15:09:37 +0500

    Partial Gameflow implementation

---
+++ Assets/TileMonsterHunter/Scripts/Abstractions/IGameFlow.cs
@@ -0,0 +1,19 @@
+﻿namespace TileMonsterHunter.Abstractions
+{
+    public interface IGameFlow
+    {
+        GameState State { get; }
+
+        void LoadMainMenu();
+        void LoadLevel(long index);
+        void Exit();
+        void StartGame();
+        void RestartGame();
+        void EndGame();
+        void PlayLevel();
+        void PauseLevel();
+        void RestartLevel();
+        void WinLevel();
+        void LoseLevel();
+    }
+}
---
+++ Assets/TileMonsterHunter/Scripts/GameFlow.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using KarenKrill.StateSystem.Abstractions;
+
+namespace TileMonsterHunter
+{
+    using Abstractions;
+
+    public class GameFlow : IGameFlow
+    {
+        public GameState State => _stateSwitcher.State;
+
+        public GameFlow(IStateSwitcher<GameState> stateSwitcher)
+        {
+            _stateSwitcher = stateSwitcher;
+        }
+        public void LoadMainMenu()
+        {
+            _loadSceneAwaiter = SceneManager.LoadSceneAsync("MenuScene", LoadSceneMode.Single);
+            _loadSceneAwaiter.completed += OnMainMenuLoadCompleted;
+            //_stateSwitcher.TransitTo(GameState.SceneLoad);
+        }
+        public void LoadLevel(long index)
+        {
+            throw new System.NotImplementedException();
+        }
+        public void Exit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.ExitPlaymode();
+#else // для мобильных и веба не сработает, надо предусмотреть
+            Application.Quit();
+#endif
+        }
+        public void StartGame()
+        {
+            _loadSceneAwaiter = SceneManager.LoadSceneAsync("LevelScene", LoadSceneMode.Single);
+            _loadSceneAwaiter.completed += OnLevelLoadCompleted;
+            //_stateSwitcher.TransitTo(GameState.SceneLoad);
+        }
+        public void RestartGame()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void EndGame()
+        {
+            _stateSwitcher.TransitTo(GameState.GameEnd);
+        }
+        public void PlayLevel()
+        {
+            _stateSwitcher.TransitTo(GameState.Gameplay);
+        }
+        public void PauseLevel()
+        {
+            _stateSwitcher.TransitTo(GameState.Pause);
+        }
+        public void RestartLevel()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void WinLevel()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void LoseLevel()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private readonly IStateSwitcher<GameState> _stateSwitcher;
+        private AsyncOperation _loadSceneAwaiter;
+        private void OnMainMenuLoadCompleted(AsyncOperation obj)
+        {
+            _stateSwitcher.TransitTo(GameState.MainMenu);
+        }
+        private void OnLevelLoadCompleted(AsyncOperation obj)
+        {
+            _stateSwitcher.TransitTo(GameState.Gameplay);
+        }
+    }
+}
