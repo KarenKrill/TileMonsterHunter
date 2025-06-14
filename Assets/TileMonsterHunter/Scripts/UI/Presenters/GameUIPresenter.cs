902ae5c1c15af6bedd68a5c2ed46a3cb8b35b0c3
KarenKrill <vampart1997@gmail.com>
2025-06-15 04:15:29 +0500

    Added tile view and hotbar view

--- Assets/TileMonsterHunter/Scripts/UI/Presenters/GameUIPresenter.cs
+++ Assets/TileMonsterHunter/Scripts/UI/Presenters/GameUIPresenter.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 using KarenKrill.UI.Presenters.Abstractions;
 using KarenKrill.UI.Views.Abstractions;
@@ -6,6 +9,7 @@ using KarenKrill.UI.Views.Abstractions;
 namespace TileMonsterHunter.UI.Presenters
 {
     using Abstractions;
+    using TileMonsterHunter.Abstractions;
     using Views.Abstractions;
 
     public class GameUIPresenter : PresenterBase<IGameUIView>, IGameUIPresenter, IPresenter<IGameUIView>
@@ -15,19 +19,78 @@ namespace TileMonsterHunter.UI.Presenters
 #nullable restore
 
         public GameUIPresenter(IViewFactory viewFactory,
-            IPresenterNavigator navigator) : base(viewFactory, navigator)
+            IPresenterNavigator navigator,
+            ITileHotBar tileHotBar) : base(viewFactory, navigator)
         {
+            _tileHotBar = tileHotBar;
         }
 
         protected override void Subscribe()
         {
             View.RefreshRequested += OnRefreshRequested;
+            _tileHotBar.MaxLengthChanged += OnHotBarMaxLengthChanged;
+            _tileHotBar.TileAdded += OnHotBarTileAdded;
+            _tileHotBar.TilesRemoved += OnHotBarTilesRemoved;
+            _tileHotBar.TilesCleared += OnHotBarTilesCleared;
         }
         protected override void Unsubscribe()
         {
             View.RefreshRequested -= OnRefreshRequested;
+            _tileHotBar.MaxLengthChanged -= OnHotBarMaxLengthChanged;
+            _tileHotBar.TileAdded -= OnHotBarTileAdded;
+            _tileHotBar.TilesRemoved -= OnHotBarTilesRemoved;
         }
 
+        private readonly ITileHotBar _tileHotBar;
+
+        private readonly Dictionary<TileInfo, int> _hotBarTileIndices = new();
+        private int _lastHotBarIndex = -1;
         private void OnRefreshRequested() => RefreshRequested?.Invoke();
+        private void OnHotBarMaxLengthChanged(int maxLength)
+        {
+            View.TilesHotBarLength = maxLength;
+        }
+        private void OnHotBarTileAdded(TileInfo tileInfo)
+        {
+            _hotBarTileIndices[tileInfo] = ++_lastHotBarIndex;
+            View.SetHotBarTile(_lastHotBarIndex, tileInfo.ShapeSprite, tileInfo.FaceSprite, tileInfo.Color);
+        }
+        private void OnHotBarTilesRemoved(TileInfo[] tilesInfo)
+        {
+            foreach (var tileInfo in tilesInfo)
+            {
+                var index = _hotBarTileIndices[tileInfo];
+                View.SetHotBarTile(index, null, null, Color.clear);
+                _hotBarTileIndices.Remove(tileInfo);
+            }
+            RefreshIndices();
+        }
+        private void OnHotBarTilesCleared()
+        {
+            foreach (var tileIndex in _hotBarTileIndices.Values)
+            {
+                View.SetHotBarTile(tileIndex, null, null, Color.clear);
+            }
+            _hotBarTileIndices.Clear();
+            _lastHotBarIndex = -1;
+        }
+
+        private void RefreshIndices()
+        {
+            // Clear old positions:
+            foreach (var index in _hotBarTileIndices.Values)
+            {
+                View.SetHotBarTile(index, null, null, Color.clear);
+            }
+            var tilesInfo = _hotBarTileIndices.Keys.ToList();
+            _hotBarTileIndices.Clear();
+            _lastHotBarIndex = -1;
+            // Show new positions:
+            foreach (var tileInfo in tilesInfo)
+            {
+                _hotBarTileIndices[tileInfo] = ++_lastHotBarIndex;
+                View.SetHotBarTile(_lastHotBarIndex, tileInfo.ShapeSprite, tileInfo.FaceSprite, tileInfo.Color);
+            }
+        }
     }
 }
--- Assets/TileMonsterHunter/Scripts/UI/Views/GameUIView.cs
+++ Assets/TileMonsterHunter/Scripts/UI/Views/GameUIView.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,12 +11,32 @@ namespace TileMonsterHunter.UI.Views
 
     public class GameUIView : ViewBehaviour, IGameUIView
     {
+        public int TilesHotBarLength
+        {
+            set
+            {
+                foreach (var tileView in _tileViews)
+                {
+                    tileView.ShapeSprite = null;
+                    tileView.FaceSprite = null;
+                    tileView.Color = Color.clear;
+                    tileView.gameObject.SetActive(false);
+                }
+                for (int i = 0; i < value && i < _tileViews.Count; i++)
+                {
+                    _tileViews[i].gameObject.SetActive(true);
+                }
+            }
+        }
+
 #nullable enable
         public event Action? RefreshRequested;
 #nullable restore
 
         [SerializeField]
         private Button _refreshButton;
+        [SerializeField]
+        private List<TileView> _tileViews = new();
 
         private void OnEnable()
         {
@@ -27,5 +48,11 @@ namespace TileMonsterHunter.UI.Views
         }
 
         private void OnRefreshButtonClicked() => RefreshRequested?.Invoke();
+        public void SetHotBarTile(int index, Sprite shapeSprite, Sprite faceSprite, Color color)
+        {
+            _tileViews[index].ShapeSprite = shapeSprite;
+            _tileViews[index].FaceSprite = faceSprite;
+            _tileViews[index].Color = color;
+        }
     }
 }
